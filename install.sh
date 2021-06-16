0cdbc2a4a022c4c19a4eb1e6fdd38e0dc14d35db
Pradhipta Bagaskara <Pradhiptabagaskara@gmail.com>
2021-06-16 11:18:02 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+apt-get update -y
+apt-get install python3.7 -y
+wget https://bootstrap.pypa.io/get-pip.py
+python3.7 get-pip.py
+pip install virtualenv
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common - y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
+curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb
+dpkg -i gitlab-runner_amd64.deb
+usermod -aG docker gitlab-runner
+echo "gitlab-runner ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+> /home/gitlab-runner/.bash_logout
