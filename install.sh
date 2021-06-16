7f2b08870a3e2e51fe98c81cf9a56e598332f092
Pradhipta Bagaskara <Pradhiptabagaskara@gmail.com>
2021-06-16 16:40:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt-get install python3.7 -y
 wget https://bootstrap.pypa.io/get-pip.py
 python3.7 get-pip.py
 pip install virtualenv
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common - y
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
